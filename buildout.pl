5afc05390937c1c703d3808bae507e430d335b9d
justingit <perl@justinsimoni.com>
2016-11-26 13:07:07 -0700

    THEMES FILES!

--- buildout.pl
+++ buildout.pl
@@ -6,13 +6,28 @@ use File::Copy::Recursive;
 my $distros = { 
 
 	default => { 
-		assets => 'assets-bak',
+		theme_file => 'theme-bak',
 		target_dir => 'default',
 	},
-	ddm_red => { 
-		assets     => 'assets-ddm_red',
-		target_dir => 'ddm_red',
+	car_camping => { 
+		theme_file     => 'theme-car_camping',
+		target_dir     => 'car_camping',
 	},
+	chocolate_love => { 
+		theme_file     => 'theme-chocolate_love',
+		target_dir     => 'chocolate_love',
+	},
+	dm_deep_red => { 
+		theme_file     => 'theme-dm_deep_red',
+		target_dir => 'dm_deep_red',
+	},
+	ocean_dream => { 
+		theme_file => 'theme-ocean_dream',
+		target_dir => 'ocean_dream',
+	},
+	
+	
+
 };
 
 
@@ -20,28 +35,36 @@ my $working_dir = './';
 chdir($working_dir);
 
 
-f_dircopy('src/assets', 'src/assets-bak');
+f_cp('theme.scss', 'theme-bak.scss');
 
 foreach my $d(keys %$distros){ 
-	my $assets_dir = $distros->{$d}->{assets};
+
+	my $theme_file = $distros->{$d}->{theme_file} . '.scss';
 	my $target_dir = $distros->{$d}->{target_dir};
 	
-	f_dircopy( 'src/' . $assets_dir, 'src/assets');
+	f_cp( $theme_file, 'theme.scss');
 	print `npm run noninteractive`;
 	
 	if(-d 'to_bundle/' . $target_dir){ 
 		`rm -rf to_bundle/$target_dir`;
 	}
-	
 	mkdir('to_bundle/' . $target_dir);
+
 	f_dircopy('dist', 'to_bundle/' . $target_dir . '/dist'); 
-	`rm -rf src/assets`;
+	`rm theme.scss`;
 }
 
-f_dircopy('src/assets-bak', 'src/assets'); 
+f_cp('theme-bak.scss', 'theme.scss');
 
-`rm -rf src/assets-bak`;
+unlink('theme-bak.scss') or warn $!; 
 
+
+sub f_cp {
+    require File::Copy;
+    my ( $to, $from ) = @_;
+    my $r = File::Copy::copy( $to, $from );    # or croak "Copy failed: $!";
+    return $r;
+}
 
 
 sub f_dircopy {
