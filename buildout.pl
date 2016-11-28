c27dda0bcd5fce033cfea8c7776c49b86917e119
justingit <perl@justinsimoni.com>
2016-11-27 18:39:52 -0700

    Themes!

--- buildout.pl
+++ buildout.pl
@@ -3,12 +3,17 @@
 use File::Copy; 
 use File::Copy::Recursive;
 
+my $scss_dir    = 'src/assets/scss'; 
+my $working_dir = './';
+
+
 my $distros = { 
 
 	default => { 
 		theme_file => 'theme-bak',
 		target_dir => 'default',
 	},
+	
 	car_camping => { 
 		theme_file     => 'theme-car_camping',
 		target_dir     => 'car_camping',
@@ -17,32 +22,71 @@ my $distros = {
 		theme_file     => 'theme-chocolate_love',
 		target_dir     => 'chocolate_love',
 	},
+	
 	dm_deep_red => { 
 		theme_file     => 'theme-dm_deep_red',
 		target_dir => 'dm_deep_red',
 	},
+	
+	flamingo_park => { 
+		theme_file     => 'theme-flamingo_park',
+		target_dir     => 'flamingo_park',
+	},
+	
+	holiday_hello_candy_cane => { 
+		theme_file     => 'theme-holiday_hello_candy_cane',
+		target_dir     => 'holiday_hello_candy_cane',
+	},
+	
+	minty_fresh => { 
+		theme_file     => 'theme-minty_fresh',
+		target_dir     => 'minty_fresh',
+	},
+		
+	nonviolent_violet => { 
+		theme_file     => 'theme-nonviolent_violet',
+		target_dir     => 'nonviolent_violet',
+	},
+
 	ocean_dream => { 
 		theme_file => 'theme-ocean_dream',
 		target_dir => 'ocean_dream',
 	},
 	
+	salmon_are_running => { 
+		theme_file     => 'theme-salmon_are_running',
+		target_dir     => 'salmon_are_running',
+	},
+	
+	techwear_in_the_alps => { 
+		theme_file     => 'theme-techwear_in_the_alps',
+		target_dir     => 'techwear_in_the_alps',
+	},
 	
 
 };
 
-
-my $working_dir = './';
 chdir($working_dir);
 
 
-f_cp('theme.scss', 'theme-bak.scss');
+f_cp(
+	$scss_dir . '/theme.scss', 
+	$scss_dir . '/theme-bak.scss'
+);
 
 foreach my $d(keys %$distros){ 
-
+	
 	my $theme_file = $distros->{$d}->{theme_file} . '.scss';
 	my $target_dir = $distros->{$d}->{target_dir};
 	
-	f_cp( $theme_file, 'theme.scss');
+	print '$theme_file: ' . $theme_file . "\n";
+	print '$target_dir: ' . $target_dir . "\n";
+	
+	f_cp( 
+		$scss_dir . '/' . $theme_file, 
+		$scss_dir . '/' . 'theme.scss'
+	);
+	
 	print `npm run noninteractive`;
 	
 	if(-d 'to_bundle/' . $target_dir){ 
@@ -50,13 +94,21 @@ foreach my $d(keys %$distros){
 	}
 	mkdir('to_bundle/' . $target_dir);
 
-	f_dircopy('dist', 'to_bundle/' . $target_dir . '/dist'); 
-	`rm theme.scss`;
+	f_dircopy(
+		'dist', 
+		'to_bundle/' . $target_dir . '/dist'
+	); 
+	
+	unlink($scss_dir . '/' . 'theme.scss');
 }
 
-f_cp('theme-bak.scss', 'theme.scss');
+f_cp(
+	$scss_dir . '/theme-bak.scss', 
+	$scss_dir . '/theme.scss'
+);
 
-unlink('theme-bak.scss') or warn $!; 
+unlink($scss_dir . '/theme-bak.scss') 
+	or warn $!; 
 
 
 sub f_cp {
