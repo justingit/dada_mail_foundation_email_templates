cfd23a50517413491c1e0c4ef5a27a826bab39f4
justingit <perl@justinsimoni.com>
2019-05-28 20:30:25 -0600

    Closes #10

--- buildout.pl
+++ buildout.pl
@@ -22,79 +22,83 @@ my $distros = {
 		theme_file => 'theme-bak',
 		target_dir => 'default',
 	},
-
-bird_of_paradise => { 
-	theme_file     => 'theme-bird_of_paradise',
-	target_dir     => 'bird_of_paradise',
-},
-
-car_camping => { 
-	theme_file     => 'theme-car_camping',
-	target_dir     => 'car_camping',
-},
-
-chocolate_love => { 
-	theme_file     => 'theme-chocolate_love',
-	target_dir     => 'chocolate_love',
-},
-
-digging_in_the_dirt => { 
-	theme_file     => 'theme-digging_in_the_dirt',
-	target_dir => 'digging_in_the_dirt',
-
-},
-
-dm_deep_red => { 
-	theme_file     => 'theme-dm_deep_red',
-	target_dir => 'dm_deep_red',
-},
-
-flamingo_park => { 
-	theme_file     => 'theme-flamingo_park',
-	target_dir     => 'flamingo_park',
-},
-
-holiday_hello_candy_cane => { 
-	theme_file     => 'theme-holiday_hello_candy_cane',
-	target_dir     => 'holiday_hello_candy_cane',
-},
-
-minty_fresh => { 
-	theme_file     => 'theme-minty_fresh',
-	target_dir     => 'minty_fresh',
-},
 	
-nonviolent_violet => { 
-	theme_file     => 'theme-nonviolent_violet',
-	target_dir     => 'nonviolent_violet',
-},
+	bird_of_paradise => { 
+		theme_file     => 'theme-bird_of_paradise',
+		target_dir     => 'bird_of_paradise',
+	},
 
-ocean_dream => { 
-	theme_file => 'theme-ocean_dream',
-	target_dir => 'ocean_dream',
-},
+	car_camping => { 
+		theme_file     => 'theme-car_camping',
+		target_dir     => 'car_camping',
+	},
 
-plain_white_t => {
-	theme_file => 'theme-plain_white_t',
-	target_dir => 'plain_white_t',
-},
+	chocolate_love => { 
+		theme_file     => 'theme-chocolate_love',
+		target_dir     => 'chocolate_love',
+	},
 
-salmon_are_running => { 
-	theme_file     => 'theme-salmon_are_running',
-	target_dir     => 'salmon_are_running',
-},
+	digging_in_the_dirt => { 
+		theme_file     => 'theme-digging_in_the_dirt',
+		target_dir => 'digging_in_the_dirt',
 
-summer_is_coming => { 
-	theme_file     => 'theme-summer_is_coming',
-	target_dir     => 'summer_is_coming',
-},
+	},
 
-techwear_in_the_alps => { 
-	theme_file     => 'theme-techwear_in_the_alps',
-	target_dir     => 'techwear_in_the_alps',
-},
+	dm_deep_red => { 
+		theme_file     => 'theme-dm_deep_red',
+		target_dir => 'dm_deep_red',
+	},
+
+	flamingo_park => { 
+		theme_file     => 'theme-flamingo_park',
+		target_dir     => 'flamingo_park',
+	},
+
+	holiday_hello_candy_cane => { 
+		theme_file     => 'theme-holiday_hello_candy_cane',
+		target_dir     => 'holiday_hello_candy_cane',
+	},
+
+	minty_fresh => { 
+		theme_file     => 'theme-minty_fresh',
+		target_dir     => 'minty_fresh',
+	},
+	
+	nonviolent_violet => { 
+		theme_file     => 'theme-nonviolent_violet',
+		target_dir     => 'nonviolent_violet',
+	},
+
+	ocean_dream => { 
+		theme_file => 'theme-ocean_dream',
+		target_dir => 'ocean_dream',
+	},
+
+	plain_white_t => {
+		theme_file => 'theme-plain_white_t',
+		target_dir => 'plain_white_t',
+	},
+
+	salmon_are_running => { 
+		theme_file     => 'theme-salmon_are_running',
+		target_dir     => 'salmon_are_running',
+	},
+
+	summer_is_coming => { 
+		theme_file     => 'theme-summer_is_coming',
+		target_dir     => 'summer_is_coming',
+	},
+
+	techwear_in_the_alps => { 
+		theme_file     => 'theme-techwear_in_the_alps',
+		target_dir     => 'techwear_in_the_alps',
+	},
 
 }; 
+
+
+
+	
 
 chdir($working_dir);
 
@@ -124,10 +128,19 @@ foreach my $d(keys %$distros){
 	}
 	mkdir('to_bundle/' . $target_dir);
 
+
+	# This puts back the, "none" file to actually not have styling: 
+	f_cp(
+		'src/pages/mailing_list_message-none.html',
+		'dist/mailing_list_message-none.html',
+	);
+	
 	f_dircopy(
 		'dist', 
 		'to_bundle/' . $target_dir . '/dist'
 	); 
+	
+	
 	
 	unlink($scss_dir . '/' . 'theme.scss');
 }
